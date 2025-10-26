25f6ed70d80382702d4772e2358dfbd036ceaf94
salokh1dd1n <berdiyarovsalohiddin@gmail.com>
2025-10-26 23:11:09 +0000

    Added view

---
+++ mvcNET/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvcNET.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+    
+}
